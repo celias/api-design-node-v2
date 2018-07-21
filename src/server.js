c17c0176209601909f6e06c8ff36558ab4fd0571
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-07-21 16:51:30 -0500

    middleware

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import express from 'express'
 import setupMiddware from './middleware'
 import { restRouter } from './api'
-// import { connect } from './db'
+import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
 // Declare an app from express
 const app = express()
 
 setupMiddware(app)
-// connect()
+connect()
 // setup basic routing for index route
 
 app.use('/signin', signin)
-app.use('/api', restRouter)
+app.use('/api', function(req, res, next) {
+  console.log("hello from middleware")
+  next()
+}, restRouter)
 // catch all
 app.all('*', (req, res) => {
   res.json({ok: true})
